4459fee5d2b55a013b1e1a2d912124fe39748095
Justin Merz <jrmerz@gmail.com>
2021-03-17 11:09:48 -0700

    impl base styles for browse-by pages, #72

--- services/ucd-lib-client/client/public/elements/styles/shared-styles.js
+++ services/ucd-lib-client/client/public/elements/styles/shared-styles.js
@@ -5,7 +5,8 @@ import damsCss from "./dams-styles.css";
 
 // Insert campus and DAMS style properties for polymer elements
 let styleEle = document.createElement('style');
-styleEle.innerHTML = ucdCssProps + damsCssProps;
+styleEle.id = 'foobar';
+styleEle.innerHTML = ucdCss + ucdCssProps + damsCssProps;
 document.head.appendChild(styleEle);
 
 // Insert campus and DAMS style rules for polymer elements
