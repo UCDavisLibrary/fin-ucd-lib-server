0b09d897cd27925c10e6e32024fe16f67dcebba3
Dusty Cartwright <103536473+UcDust@users.noreply.github.com>
2023-01-12 10:27:49 -0800

    append .zip in stream header, add regex for file naming (#109)
    * append .zip in stream header, add regex in client side to make filename more robust
    * move regex naming to streaming service

--- services/ucd-lib-client/lib/http-zip-stream.js
+++ services/ucd-lib-client/lib/http-zip-stream.js
@@ -18,10 +18,12 @@ class HttpZipStream {
    */
   zip(res, zipName, urls) {
     let resolved = false;
+    zipName = zipName.replace(/[^a-zA-Z0-9]+/g, '_').replace(/^_*\//).replace(/_*$\//).toLowerCase();
+    
     return new Promise(async (resolve, reject) => {
       
       res.setHeader('content-type', 'application/zip');
-      res.setHeader('content-disposition', `attachment; filename="${zipName}"`)
+      res.setHeader('content-disposition', `attachment; filename="${zipName}.zip"`)
 
       let archive = archiver('zip', {
         zlib: { level: 9 } // Sets the compression level.
